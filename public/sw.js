f4c318a4288127854018c9c8567da9f9bf20e907
hindiakshar0702-star <hindiakshar.0702@gmail.com>
2026-06-26 20:44:58 +0530

    feat: integrate custom Monetag ad tag zone 11204099 and update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11203691
+    "domain": "nap5k.com",
+    "zoneId": 11204099
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://nap5k.com/act/files/service-worker.min.js?r=sw')
